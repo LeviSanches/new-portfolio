784eebc6da5353617737bb4506c0571dd7fbf9d0
Lais Feitosa <levi_sanches@outlook.com>
2023-06-21 15:18:10 -0300

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("meuBotao");
+  
+    botao.addEventListener("click", function(event) {
+      console.log("O botão foi clicado!");
+    });
+  });
