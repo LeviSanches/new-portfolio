89b6ad383c5c95d66691ebc2ee99031f8365e5ae
Levi Sanches <levi_sanches@outlook.com>
2023-06-25 14:26:04 -0300

    finish

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botao = document.getElementById("meuBotao");
-  
-    botao.addEventListener("click", function(event) {
-      console.log("O botão foi clicado!");
-    });
-  });
+let main = document.getElementById('main');
+let portfolio = document.getElementById('portfolio');
+let about = document.getElementById('about');
+let skills = document.getElementById('skills');
+let navMobile = document.getElementById('nav-mobile')
+
+function showSection(sectionId) {
+    let sections = document.getElementsByTagName('section');
+
+    for (let i = 0; i < sections.length; i++) {
+        let section = sections[i];
+
+        if (section.id === sectionId) {            
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    }
+}
+
+function buttonNav() {
+    if (navMobile.style.display === 'block') {
+      navMobile.style.display = 'none';
+    } else {
+      navMobile.style.display = 'block';
+    }
+  }
